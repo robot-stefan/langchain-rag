a3835a4d8afc4a34d372c89163068e71e94b8f72
robot-stefan <160691166+robot-stefan@users.noreply.github.com>
2025-05-09 18:45:02 -0400

    Update populate_database.py
    Added moving files to _added folder once done chunking, added cloud processing of unstructured.

--- populate_database.py
+++ populate_database.py
@@ -1,14 +1,16 @@
-import argparse, shutil, io, os, base64
-
+import argparse, shutil, io, os, base64, requests
 from langchain import hub
 from langchain_core.documents import Document
 from langchain_chroma import Chroma
 from langchain_unstructured import UnstructuredLoader
+from unstructured_client import UnstructuredClient
+from unstructured_client.utils import BackoffStrategy, RetryConfig
 from langchain_community.vectorstores.utils import filter_complex_metadata
 from langchain_community.document_loaders.parsers import LLMImageBlobParser
 from langchain_ollama import OllamaLLM
 
 from config import configSetting
+from file_operations import filesList, fileMoveToAdded
 
 def main():
 
@@ -19,6 +21,8 @@ def main():
     parser.add_argument("--add_ollama", action="store_true", 
                         help="Build the database using ollama.")
     parser.add_argument("--add_mistral", action="store_true", 
+                        help="Build the database using mistral cloud.")
+    parser.add_argument("--add_CLOUD_ONLY", action="store_true", 
                         help="Build the database using mistral cloud.")
     parser.add_argument("--docs_folder", action="store", type=str, default=configSetting.documentPath, 
                         help="Folder where documents are located.")
@@ -33,42 +37,79 @@ def main():
         print("--> Wipe Complete !!!")
 
     elif args.add_ollama:
+        # Use unstructured locally and ollama for encoding
+        cloudUnstructured = True
         print("--> Adding Documents to Database")
-        buildChromadb(args.docs_folder, args.chroma_folder, configSetting.embeddings)
+        buildChromadb(args.docs_folder, args.chroma_folder, configSetting.embeddings, cloudUnstructured)
         print("--> Adding Documents Complete !!!")
     
-    # Comment out below elif to disable cloud option
     elif args.add_mistral:
-        from configCloud import configCloudSetting # Comment out to disable clould option
+        # Use unstructured locally and mistral cloud for encoding
+        from configCloud import configCloudSetting
+        cloudUnstructured = False
         print("--> Adding Documents to Database")
-        buildChromadb(args.docs_folder, args.chroma_folder, configCloudSetting.embeddingsCloudMistral)
+        buildChromadb(args.docs_folder, args.chroma_folder, configCloudSetting.embeddingsCloudMistral, cloudUnstructured)
+        print("--> Adding Documents Complete !!!")
+    
+    elif args.add_CLOUD_ONLY:
+        # Use unstructured cloud and mistral cloud for encoding
+        from configCloud import configCloudSetting
+        cloudUnstructured = True
+        print("--> Adding Documents to Database")
+        buildChromadb(args.docs_folder, args.chroma_folder, configCloudSetting.embeddingsCloudMistral, cloudUnstructured)
         print("--> Adding Documents Complete !!!")
 
+def fileLoad(file, cloud):
+    if cloud == False:
+        loader = UnstructuredLoader(
+            file_path=file,
+            strategy="hi_res",
+            mode="elements",
+            add_start_index=True,
+            chunking_strategy="by_title",
+            max_characters=1500,
+            multipage_sections=True,
+            # show_progress_bar=True,
+            # infer_table_structure=True,
+            hi_res_model_name="yolox"
+        )
+    elif cloud == True:
+        from configCloud import UNSTRUCTURED_API_KEY
 
-def filesList(documentPath):
-    # Create list of files in directory and sorts them abc
-    filesList = os.listdir(documentPath)
-    fileList.sort()
-    return [documentPath + "/" + x  for x in filesList if os.path.isfile(os.path.join(documentPath, x))]
+        client = UnstructuredClient(
+            api_key_auth=UNSTRUCTURED_API_KEY, 
+            # client=requests.Session(),  # Define your own requests session
+            server_url="https://api.unstructuredapp.io/general/v0/general",  # Define your own api url
+            retry_config=RetryConfig(
+                strategy="backoff",
+                retry_connection_errors=True,
+                backoff=BackoffStrategy(
+                    initial_interval=500,
+                    max_interval=60000,
+                    exponent=1.5,
+                    max_elapsed_time=900000,
+                ),
+            ),  
+        )
 
-def fileLoad(file):
-    loader = UnstructuredLoader(
-        file_path=file,
-        strategy="hi_res",
-        mode="elements",
-        add_start_index=True,
-        chunking_strategy="by_title",
-        max_characters=1500,
-        multipage_sections=True,
-        # show_progress_bar=True,
-        # infer_table_structure=True,
-        hi_res_model_name="yolox"
-    )
+        loader = UnstructuredLoader(
+            file_path=file,
+            client=client,
+            partition_via_api=True,
+            strategy="hi_res",
+            mode="elements",
+            add_start_index=True,
+            chunking_strategy="by_title",
+            max_characters=1500,
+            multipage_sections=True,
+            # show_progress_bar=True,
+            # infer_table_structure=True,
+            hi_res_model_name="yolox"
+        )
 
     return loader.load()
 
 def calculate_chunk_ids(chunks):
-
     # This will create IDs like "data/monopoly.pdf:6:2"
     # Page Source : Page Number : Chunk Index
 
@@ -101,7 +142,7 @@ def wipeDatabase(chromaPath):
     if os.path.exists(chromaFolder):
         shutil.rmtree(chromaFolder)
 
-def buildChromadb(documentPath, chromaPath, embeddings):
+def buildChromadb(documentPath, chromaPath, embeddings, cloudUnstructured):
     ## Add each file one by one to allow for partial success of large file batches in the event of an error. 
     docsFolder = f"{configSetting.topPath}/{documentPath}"
     chromaFolder = f"{configSetting.topPath}/{chromaPath}"
@@ -123,7 +164,7 @@ def buildChromadb(documentPath, chromaPath, embeddings):
     totalFileCount = len(files)
     for file in files:
         print(f"--> Currently loading file: [{currentFileCounter} / {totalFileCount}] - {file}")
-        chunks = fileLoad(file)
+        chunks = fileLoad(file, cloudUnstructured)
         chunksWithId = calculate_chunk_ids(chunks)
         
         # Build list of new chunks not in database 
@@ -136,8 +177,10 @@ def buildChromadb(documentPath, chromaPath, embeddings):
             print(f"--> Encoding {len(new_chunks)} new chunks from: [{currentFileCounter} / {totalFileCount}] - {file}")
             new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
             db.add_documents(filter_complex_metadata(new_chunks), ids=new_chunk_ids)
+            fileMoveToAdded(file)
             print(f"--> Done adding chunks from: [{currentFileCounter} / {totalFileCount}] - {file}")
         else:
+            fileMoveToAdded(file)
             print(f"--> No new chunks to add from: [{currentFileCounter} / {totalFileCount}] - {file}")
 
         currentFileCounter+= 1
